1e2760251b632d7423b9fd0dd8dc7fc726d10e09
ChocoretoStonus <chocoretostonus@gmail.com>
2022-08-22 19:39:08 -0500

    as

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -3,7 +3,9 @@
 
 int main(int arg, char** argv) {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("OpenGL", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,0);
+	SDL_Window* window = SDL_CreateWindow("OpenGL", SDL_WINDOWPOS_CENTERED,
+						 SDL_WINDOWPOS_CENTERED,800,600,0);
 	SDL_Delay(6000);
+	printf("La ventana no puede ser creada %\n", SDL_GetError());
 	return 0;
 }
