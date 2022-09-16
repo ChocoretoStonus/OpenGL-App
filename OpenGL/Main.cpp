0b5d9d373b02016c4e1123988c23f4326cb8ad1b
ChocoretoStonus <chocoretostonus@gmail.com>
2022-09-15 19:59:02 -0500

    Correccion

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -1,21 +1,106 @@
 #include <stdio.h>
 #include <sdl/SDL.h>
+#include <sdl/SDL_image.h>
 #include <string>
+
 
 
 using namespace std;
 
-const int Ancho_Pantalla = 640;
-const int Largo_Pantalla = 480;
+const int Ancho_Pantalla = 800;
+const int Largo_Pantalla = 600;
 
 
+class LTextura {
+public:
+	LTextura();
+	~LTextura();
+	bool cargaArchivo(string path);
+	void liberar();
+	void render(int x, int y);
+	int obtenerAncho();
+	int obtenerAlto();
+private:
+	SDL_Texture* mTextura;
+	int mAncho;
+	int mAlto;
+};
+
+bool inicio();
+bool cargar();
+void cerrar();
 SDL_Window* ventana = NULL;
-SDL_Surface* pantallaSuperficie = NULL;
-SDL_Surface* SuperficieAjustada = NULL;
-SDL_Surface* ImagenCargada = NULL;
-SDL_Surface* SuperficieActual = NULL;
-SDL_Surface* cargaSuperficie (string path);
+SDL_Renderer* gRenderizado = NULL;
+LTextura gImagenTextura;
+LTextura gFondoTextura;
 
+LTextura::LTextura() {
+	mTextura = NULL;
+	mAncho = 0;
+	mAlto = 0;
+}
+
+
+LTextura::~LTextura() {
+	liberar();
+}
+
+
+bool LTextura::cargaArchivo(string path) {
+	liberar();
+	SDL_Texture* nuevaTextura = NULL;
+	SDL_Surface* cargaSuperficie = IMG_Load(path.c_str());
+	if (cargaSuperficie ==NULL)
+	{
+		printf("No se puede cargar la imagen %s! SDL_image Error: %s\n", IMG_GetError());
+	}
+	else
+	{
+		SDL_SetColorKey(cargaSuperficie,SDL_TRUE,SDL_MapRGB(cargaSuperficie->format,0,0xFF,0xFF));
+		nuevaTextura = SDL_CreateTextureFromSurface(gRenderizado, cargaSuperficie);
+		if (nuevaTextura == NULL)
+		{
+			printf("No se puede crear la textura, SDL_image Error: %s\n", IMG_GetError());
+		}
+		else
+		{
+			mAncho = cargaSuperficie->w;
+			mAlto= cargaSuperficie->h;
+		}
+		SDL_FreeSurface(cargaSuperficie);
+	
+	}
+	
+	mTextura = nuevaTextura;
+	return mTextura !=NULL;
+}
+
+
+void LTextura::liberar() {
+	if (mTextura!=NULL)
+	{
+		SDL_DestroyTexture(mTextura);
+		mTextura = NULL;
+		mAncho = 0;
+		mAlto = 0;
+	}
+}
+
+
+void LTextura::render(int x, int y) {
+	SDL_Rect renderCuadrado = {x,y,mAncho,mAlto};
+	SDL_RenderCopy(gRenderizado,mTextura,NULL,&renderCuadrado);
+}
+
+
+int LTextura::obtenerAncho() {
+	return mAncho;
+}
+
+
+int LTextura::obtenerAlto() {
+	return mAlto;
+}
 
 
 bool inicio() {
@@ -27,6 +112,13 @@ bool inicio() {
 
 	else
 	{
+
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY,"1"))
+		{
+			printf("Warning: El filtrado de textura no esta habilitado");
+		}
+
+
 		ventana = SDL_CreateWindow("SDL Ajuste Imagen", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Ancho_Pantalla, Largo_Pantalla
 			, SDL_WINDOW_SHOWN);
 		if (ventana == NULL)
@@ -35,9 +127,25 @@ bool inicio() {
 		}
 		else
 		{
-			pantallaSuperficie = SDL_GetWindowSurface(ventana);
+			gRenderizado = SDL_CreateRenderer(ventana,-1,SDL_RENDERER_ACCELERATED);
+			if (gRenderizado == NULL)
+			{
+				printf("El renderizado no puede ser creado, SDL ERROR: %s\n", SDL_GetError());
+				suceso = false;
+			}
+			else
+			{
+				SDL_SetRenderDrawColor(gRenderizado, 0xFF, 0xFF, 0xFF, 0xFF);
+				int imagenBandera = IMG_INIT_PNG;
+
+				if (!(IMG_Init(imagenBandera) & imagenBandera))
+				{
+					printf("SDL_image could not initialize! SDL_image Error: %s\n",
+						IMG_GetError());
+					suceso = false;
+				}
+			}
 		}
-		SDL_ShowCursor(0);
 		return suceso;
 	}
 }
@@ -48,9 +156,13 @@ bool cargar() {
 	
 	bool suceso = true;
 
-	SuperficieAjustada = cargaSuperficie("img/right.bmp");
-	
-	if (SuperficieAjustada == NULL)
+	if (!gImagenTextura.cargaArchivo("img/uwu.png"))
+	{
+		printf("Es imposible cargar la imgane hagalo bien");
+		suceso = false;
+	}
+
+	if (!gImagenTextura.cargaArchivo("img/uwu_.png"))
 	{
 		printf("Es imposible cargar la imgane hagalo bien");
 		suceso = false;
@@ -61,85 +173,79 @@ bool cargar() {
 
 
 void cerrar() {
-	SDL_FreeSurface(SuperficieAjustada);
-	SuperficieAjustada = NULL;	
 
+	//SDL_DestroyTexture(gTexura);
+	//SDL_DestroyRenderer(gRenderizado);
+	gImagenTextura.liberar();
+	gFondoTextura.liberar();
+	SDL_DestroyRenderer(gRenderizado);
 	SDL_DestroyWindow(ventana);
+	
 	ventana = NULL;
-	SuperficieAjustada = NULL;	
+	gRenderizado = NULL;
+	//gTexura = NULL;
 
+	IMG_Quit();
 	SDL_Quit();
 }
 
-
-SDL_Surface* cargaSuperficie(string path) {
-	
-	SDL_Surface* superficieOptimizada = NULL;
-	SDL_Surface* superficieCargada = SDL_LoadBMP(path.c_str());
-	if (superficieCargada == NULL)
-	{
-		printf("Imposible cargar la imagen %s! SDL ERROR: %s\n", 
-			path.c_str(), SDL_GetError());
-	}
-	else
-	{
-		superficieOptimizada = SDL_ConvertSurface(superficieCargada,
-							   pantallaSuperficie->format,0);
-	
-		if (superficieOptimizada==NULL)
-		{
-			printf("Imposible cargar la imagen %s! SDL ERROR: %s\n",
-				path.c_str(), SDL_GetError());
-		}
-		SDL_FreeSurface(superficieOptimizada);
-	}
-	return superficieCargada;
-}
-
-
 	
 int main(int arg, char** argv) {
-	if (!inicio())
-	{
-		printf("Fallo al iniciar!!\n");
-	}
-	else
-	{
-		if (!cargar())
+		if (!inicio())
 		{
-			printf("Fallo al cargar la imagen!!\n");
-	i9o0	}
+			printf("Fallo al iniciar!!\n");
+		}
 		else
 		{
-
-			bool salir = false;
-			SDL_Event e;
-
-			while (!salir)
+			if (false)
 			{
-				while (SDL_PollEvent(&e) != 0)
-				{
-					if (e.type == SDL_QUIT)
-					{
-						salir = true;
-					}
-				}
-					SDL_Rect rectaAjustada;
-					rectaAjustada.x = 0;
-					rectaAjustada.y = 0;
-					rectaAjustada.w = Ancho_Pantalla;
-					rectaAjustada.h = Largo_Pantalla;
+				printf("Fallo al cargar la imagen!!\n");
+			}
+			else
+			{
 
-					SDL_BlitScaled(SuperficieAjustada,NULL, 
-									pantallaSuperficie, &rectaAjustada);
-					SDL_UpdateWindowSurface(ventana);
-			
+				bool salir = false;
+				SDL_Event e;
+
+				while (!salir)
+				{
+					while (SDL_PollEvent(&e) != 0)
+					{
+						if (e.type == SDL_QUIT)
+						{
+							salir = true;
+						}
+					}
+					SDL_SetRenderDrawColor(gRenderizado, 0xFF, 0xFF, 0xFF, 0xFF);
+					SDL_RenderClear(gRenderizado);
+					gFondoTextura.render(0,0);
+					gImagenTextura.render(240,190);
+
+
+
+					/*SDL_Rect llenarRectangulo = { Ancho_Pantalla / 4,Largo_Pantalla / 4,Ancho_Pantalla / 2,Largo_Pantalla / 2 };
+
+					SDL_SetRenderDrawColor(gRenderizado, 0xFF, 0xFF, 0xFF, 0xFF);
+					SDL_RenderFillRect(gRenderizado, &llenarRectangulo);
+
+					SDL_Rect controlRectangulo = { Ancho_Pantalla / 6,Largo_Pantalla / 6,Ancho_Pantalla * 2 / 3,Largo_Pantalla * 2 / 3 };
+					SDL_SetRenderDrawColor(gRenderizado, 123, 764, 234, 754);
+				
+					SDL_RenderDrawRect(gRenderizado,&controlRectangulo);
+					SDL_SetRenderDrawColor(gRenderizado, 671, 188, 129, 0xFF);
+					SDL_RenderDrawLine(gRenderizado, 0,Largo_Pantalla / 2,Ancho_Pantalla,Largo_Pantalla / 2);
+					SDL_SetRenderDrawColor(gRenderizado, 623, 012, 234, 0xFF);
+					for (int i = 0; i < Largo_Pantalla; i+=4)
+					{
+						SDL_RenderDrawPoint(gRenderizado,Ancho_Pantalla/2,i);
+					}*/
+					SDL_RenderPresent(gRenderizado);
+				}
 			}
 		}
-	}
 
-	cerrar();
-	return 0;
-}
+		cerrar();
+		return 0;
+	}
 
 
