4fb2c3e4506deacfdca5e31d3e15d973158a7abe
ChocoretoStonus <chocoretostonus@gmail.com>
2022-10-11 01:42:02 -0500

    Mouse Events

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <sdl/SDL.h>
 #include <sdl/SDL_image.h>
 #include <sdl/SDL_ttf.h>
+#include <stdio.h>
 #include <string>
 
 
@@ -10,12 +10,24 @@ using namespace std;
 const int Ancho_Pantalla = 640;
 const int Largo_Pantalla = 480;
 
+const int Boton_Ancho = 300;
+const int Boton_Largo = 200;
+const int Total_Botones = 4;
+
+enum  LBotonSprite
+{
+	BOTON_SPRITE_MOUSE_SALIDA = 0,
+	BOTON_SPRITE_MOUSE_SOBRE_MOVIMIENTO = 1,
+	BOTON_SPRITE_MOUSE_ABAJO = 2,
+	BOTON_SPRITE_MOUSE_ARRIBA = 3,
+	BOTON_SPRITE_TOTAL = 4
+};
+
 class LTextura {
 public:
 	LTextura();
 	~LTextura();
 	bool cargaArchivo(string path);
-	bool cargarRenderTexto(string texturaTexto, SDL_Color textoColor);
 	void liberar();
 	void render(int x, int y, SDL_Rect* clip = NULL);
 	int obtenerAncho();
@@ -26,6 +38,19 @@ private:
 	int mAlto;
 };
 
+class LBoton
+{
+public:
+	LBoton();
+	void asignaPosicion(int x, int y);
+	void controlaEvento(SDL_Event* e);
+	void render();
+private:
+	SDL_Point mPosicion;
+	LBotonSprite mActualSprite;
+};
+
+
 bool inicio();
 bool cargar();
 void cerrar();
@@ -33,8 +58,10 @@ void cerrar();
 SDL_Surface* gTeclaCambioSuperficie;
 SDL_Window* ventana = NULL;
 SDL_Renderer* gRenderizado = NULL;
-TTF_Font* gFuente = NULL;
-LTextura gTextoTextura;
+LTextura gBotonSpriteTextura;
+SDL_Rect gSpriteClips[BOTON_SPRITE_TOTAL];
+
+LBoton gBotones[Total_Botones];
 LTextura gFondoTextura;
 
 
@@ -80,32 +107,6 @@ bool LTextura::cargaArchivo(string path) {
 }
 
 
-bool LTextura::cargarRenderTexto(string texturaTexto, SDL_Color textoColor) {
-	liberar();
-	SDL_Surface* textoSurficie = TTF_RenderText_Solid(gFuente,texturaTexto.c_str(),textoColor);
-	if (textoSurficie ==NULL)
-	{
-		printf("No se pudo cargar la fuente");
-	}
-	else
-	{
-		mTextura = SDL_CreateTextureFromSurface(gRenderizado,textoSurficie);
-		if (mTextura==NULL)
-		{
-			printf("No se pudo crear la textura");
-		}
-		else
-		{
-			mAncho = textoSurficie->w;
-			mAlto = textoSurficie->h;
-		}
-		SDL_FreeSurface(textoSurficie);
-	}
-
-	return mTextura != NULL;
-}
-
-
 void LTextura::liberar() {
 	if (mTextura != NULL)
 	{
@@ -135,6 +136,71 @@ int LTextura::obtenerAncho() {
 
 int LTextura::obtenerAlto() {
 	return mAlto;
+}
+
+
+LBoton::LBoton()
+{
+	mPosicion.x = 0;
+	mPosicion.y = 0;
+	mActualSprite = BOTON_SPRITE_MOUSE_SALIDA;
+}
+
+
+void LBoton::asignaPosicion(int x, int y)
+{
+	mPosicion.x = x;
+	mPosicion.y = y;
+}
+
+
+void LBoton::controlaEvento(SDL_Event* e) {
+	if (e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP)
+	{
+		int x, y;
+		SDL_GetMouseState(&x,&y);
+		bool dentro = true;
+		if (x <mPosicion.x)
+		{
+			dentro = true;
+		}
+		else if(x>mPosicion.x)
+		{
+			dentro = false;
+		}
+		else if (y < mPosicion.y)
+		{
+			dentro = false;
+		}
+		else if (y > mPosicion.y + Boton_Largo)
+		{
+			dentro = false;
+		}
+		if (!dentro)
+		{
+			mActualSprite = BOTON_SPRITE_MOUSE_SALIDA;
+		}
+	}
+	else
+	{
+		switch (e->type)
+		{
+		case SDL_MOUSEMOTION:
+			mActualSprite = BOTON_SPRITE_MOUSE_SOBRE_MOVIMIENTO;
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			mActualSprite = BOTON_SPRITE_MOUSE_ABAJO;
+			break;
+		case SDL_MOUSEBUTTONUP:
+			mActualSprite = BOTON_SPRITE_MOUSE_ARRIBA;
+			break;
+		}
+	}
+}
+
+
+void LBoton::render() {
+	gBotonSpriteTextura.render(mPosicion.x,mPosicion.y,&gSpriteClips[mActualSprite]);
 }
 
 
@@ -196,39 +262,40 @@ bool cargar() {
 
 	bool suceso = true;
 
-	gFuente = TTF_OpenFont("fonts/ComicSans.TTF",21);
-	if (gFuente == NULL)
+	if (!gBotonSpriteTextura.cargaArchivo("img/boton.png"))
 	{
-		printf("Es imposible cargar la fuente mi capo");
+		printf("Es imposible cargar la img mi capo");
 		suceso = false;
 	}
 	else
 	{
 
-		SDL_Color textoColor = {0,0,0};
-		if (!gTextoTextura.cargarRenderTexto("Uso de Comics Sans", textoColor))
+		for (int i = 0; i < BOTON_SPRITE_TOTAL; ++i)
 		{
-			printf("Tu texto no sirve pon otro");
-			suceso = false;
+			gSpriteClips[i].x = 0;
+			gSpriteClips[i].y = i * 200;
+			gSpriteClips[i].w = Boton_Ancho;
+			gSpriteClips[i].h = Boton_Largo;
 		}
+		gBotones[0].asignaPosicion(0,0);
+		gBotones[1].asignaPosicion(Ancho_Pantalla-Boton_Ancho,0);
+		gBotones[2].asignaPosicion(0,Largo_Pantalla-Boton_Largo);
+		gBotones[3].asignaPosicion(Ancho_Pantalla - Boton_Ancho, 
+								   Largo_Pantalla - Boton_Largo);
 
-		return suceso;
 	}
+		return suceso;
 
 }
 
 
 void cerrar() {
-
-	gTextoTextura.liberar();
-	TTF_CloseFont(gFuente);
-	gFuente = NULL;
+	gBotonSpriteTextura.liberar();
 	SDL_DestroyRenderer(gRenderizado);
 	SDL_DestroyWindow(ventana);
 
 	ventana = NULL;
 	gRenderizado = NULL;
-	TTF_Quit();
 	IMG_Quit();
 	SDL_Quit();
 }
@@ -251,7 +318,6 @@ int main(int arg, char** argv) {
 			bool salir = false;
 			SDL_Event e;
 			int frame = 0;
-
 			while (!salir)
 			{
 				while (SDL_PollEvent(&e) != 0)
@@ -260,13 +326,22 @@ int main(int arg, char** argv) {
 					{
 						salir = true;
 					}
+
+					for (int i = 0; i < Total_Botones; ++i)
+					{
+						gBotones[i].controlaEvento(&e);
+					}
 					
 				}
 
 				SDL_SetRenderDrawColor(gRenderizado, 0xFF, 0, 0xFF, 0xFF);
 				SDL_RenderClear(gRenderizado);
-				gTextoTextura.render((Ancho_Pantalla - gTextoTextura.obtenerAncho())/2, 
-									(Largo_Pantalla - gTextoTextura.obtenerAlto()) / 2);
+				
+				for (int i = 0; i < Total_Botones; ++i)
+				{
+					gBotones[i].render();
+				}
+
 				SDL_RenderPresent(gRenderizado);
 				
 			}
